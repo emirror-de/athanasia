20a9f6261a3cf9354e290219f35e26823e4ab5f8
Lewin Probst <info@emirror.de>
2021-05-26 18:15:41 +0200

    Updated Account, added transaction functions.

--- src/models/account.rs
+++ src/models/account.rs
@@ -1,13 +1,20 @@
+use {
+    crate::models::{Transaction, TransactionType},
+    log::{info, warn},
+};
+
 /// Represents an account id.
 pub type AccountId = u16;
+/// Represents the credit amount of an account.
+pub type CreditAmount = f32;
 
 /// Represents a clients account.
 #[derive(Debug)]
 pub struct Account {
     id: AccountId,
-    available: f32,
-    held: f32,
-    total: f32,
+    available: CreditAmount,
+    held: CreditAmount,
+    total: CreditAmount,
     locked: bool,
 }
 
@@ -29,22 +36,68 @@ impl Account {
     }
 
     /// Returns the available amount.
-    pub fn available(&self) -> f32 {
+    pub fn available(&self) -> CreditAmount {
         self.available
     }
 
     /// Returns the held amount.
-    pub fn held(&self) -> f32 {
+    pub fn held(&self) -> CreditAmount {
         self.held
     }
 
     /// Returns the total amount.
-    pub fn total(&self) -> f32 {
+    pub fn total(&self) -> CreditAmount {
         self.total
     }
 
     /// Returns true if the account is locked.
     pub fn locked(&self) -> bool {
         self.locked
+    }
+
+    /// Executes a deposit on the account.
+    pub fn deposit(&mut self, amount: CreditAmount) {
+        self.available += amount;
+        self.total += amount;
+        #[cfg(debug_assertions)]
+        info!("Deposit {} on account id {}", amount, self.id);
+    }
+
+    /// Executes a withdrawal on the account.
+    pub fn withdrawal(&mut self, amount: CreditAmount) -> Result<(), String> {
+        if self.available < amount {
+            let msg = format!(
+                "Withdrawal ignored on account id {}. Amount: {}",
+                self.id, amount
+            );
+            #[cfg(debug_assertions)]
+            info!("{}", msg);
+            return Err(msg);
+        }
+        self.available -= amount;
+        self.total -= amount;
+        Ok(())
+    }
+
+    /// Executes a dispute transaction. Notice that the transaction put in this
+    /// function is already the referenced one (so the one that includes the amount).
+    pub fn dispute(&mut self, transaction: &Transaction) {
+        self.available -= transaction.amount();
+        self.held += transaction.amount();
+    }
+
+    /// Executes a resolve transaction. Notice that the transaction put in this
+    /// function is already the referenced one (so the one that includes the amount).
+    pub fn resolve(&mut self, transaction: &Transaction) {
+        self.available += transaction.amount();
+        self.held -= transaction.amount();
+    }
+
+    /// Executes a chargeback transaction. Notice that the transaction put in this
+    /// function is already the referenced one (so the one that includes the amount).
+    pub fn chargeback(&mut self, transaction: &Transaction) {
+        self.held -= transaction.amount();
+        self.total -= transaction.amount();
+        self.locked = true;
     }
 }
